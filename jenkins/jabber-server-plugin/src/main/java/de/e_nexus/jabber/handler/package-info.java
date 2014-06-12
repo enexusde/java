4653b7ed667ab6695ec138d43d316d6d3b9ebda7
enexusde <webmaster@e-nexus.de>
2014-06-12 17:58:54 +0200

    Remove Jabber-Server-Plugin for Jenkins. Add eclipselink merge from old Eclipse Plugin Site.

--- jenkins/jabber-server-plugin/src/main/java/de/e_nexus/jabber/handler/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package represents the root of the Jabber-Server-Plugin. All classes
- * directly used for jabber functionality are placed here.
- */
-package de.e_nexus.jabber.handler;
-
--- jenkins/jabber-server-plugin/src/main/java/de/e_nexus/jabber/handler/vcard/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains all VCart Handlers and {@link org.apache.vysper.xmpp.modules.Module} in different versions.
- */
-package de.e_nexus.jabber.handler.vcard;
-
